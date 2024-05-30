f714bf20535ad2fb2f40479241bfe764cc9f5e98
ahujasherry18 <ahujasherry18@gmail.com>
2024-05-30 14:51:38 -0400

    add project files

--- order-service/src/main/java/com/sherryahuja/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sherryahuja.orderservice.repository;
-
-import com.sherryahuja.orderservice.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Long> {
-}
